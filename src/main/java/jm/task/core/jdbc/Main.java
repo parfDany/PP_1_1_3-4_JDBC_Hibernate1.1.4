b94d2675b2d0538b338de6e5e05adfa67781c15b
parfDany <dany991@yandex.ru>
2022-10-10 23:04:35 +0300

    поправленная версия задачи 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,10 +2,18 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
+import org.hibernate.internal.SessionFactoryImpl;
+
+import java.lang.module.Configuration;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
+
+
 
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
@@ -22,6 +30,7 @@ public class Main {
         //System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
         userService.dropUsersTable();
+
 
     }
 }
